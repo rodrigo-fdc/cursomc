8a4866ba5858d845e41f0795bc1703df137dbd11
Rodrigo Ferreira da Cunha <rodrigo@rodrigodev-security.kali>
2023-08-05 15:39:58 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/rodrigodev/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.rodrigodev.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigodev.cursomc.domain.Categoria;
+import com.rodrigodev.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
